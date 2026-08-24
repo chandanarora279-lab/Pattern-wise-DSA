11c3452684bf270f1eb7c08a174506f94d63cfa5
chandanarora279-lab <chandanarora279@gmail.com>
2026-08-24 07:41:37 +0530

    Add solution for Max Consecutive Ones III

---
+++ solutions/medium/Max Consecutive Ones III.java
@@ -0,0 +1,21 @@
+// Title: Max Consecutive Ones III
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/max-consecutive-ones-iii/
+
+                zero_count++;
+            }
+            if(zero_count>k){
+                if(nums[left]==0){
+                zero_count--;
+                }
+                left++;
+            }
+            if(nums[right]==0){
+        for(int right=0;right<n;right++){
+           
+        }
+        int zero_count=0;
+         return n-left;
+        int n=nums.length;
+    }
